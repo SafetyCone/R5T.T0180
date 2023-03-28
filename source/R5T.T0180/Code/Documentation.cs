e6d6dc908182bb72a6d6f67127a92cc5773ca3a1
davidcoats <david@getsafetycone.com>
2023-03-27 19:08:43 -0700

    Initial commit.

---
+++ source/R5T.T0180/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0180
+{
+	/// <summary>
+	/// Common file system-related strongly-typed types (IPath, IFilePath, IDirectoryPath, etc.).
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.T0180/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.T0180
+Common file system-related strongly-typed types (IPath, IFilePath, IDirectoryPath, etc.).
